fb575a9640c3d127424c937176e010c38601f798
Dat <dat@MacBook-Pro-cua-Dat.local>
2024-03-14 13:32:19 +0700

    refactor: delete top component

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -1,9 +1,8 @@
 export default function Manage() {
   return (
-    <main className={'grid grid-cols-10 grid-rows-10'}>
-      <div className={'col-start-1 col-end-11 row-start-1 row-end-3'}>Top</div>
-      <div className={'col-start-1 col-end-3 row-start-3 row-end-11'}>Sidebar</div>
-      <div className={'col-start-3 col-end-11 row-start-3 row-end-11'}>Content</div>
+    <main className={'grid grid-cols-10'}>
+      <div className={'col-start-1 col-end-2 border border-black'}>Sidebar</div>
+      <div className={'col-start-2 col-end-11 border border-black'}>Content</div>
     </main>
   )
 }
